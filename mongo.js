ef757f97c1ab0fc4b101389a34ea1bd89844c328
davidpacco <david.j.pacco@gmail.com>
2023-12-05 09:14:20 -0500

    fix command argument bug

--- mongo.js
+++ mongo.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
 
-
 if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
 
 const password = process.argv[2]
-
 const url = `mongodb+srv://fullstackopen:${password}@cluster0.pcua3or.mongodb.net/personsApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
@@ -23,10 +21,10 @@ const Person = mongoose.model('Person', personSchema)
 if (process.argv.length === 3) {
   Person.find({}).then(result => {
     console.log('phonebook:')
-    result.forEach(person => console.log(`${person.name} ${person.number}`))
+    result.forEach(person => console.log(person.name, person.number))
     mongoose.connection.close()
   })
-} else {
+} else if (process.argv.length === 5) {
   const person = new Person({
     name: process.argv[3],
     number: process.argv[4]
